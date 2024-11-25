d76246e29a0cb52cdac989c0776f6861627ca4ba
Tom Tebbe <tom.tebbe@iu-study.org>
2024-11-25 14:29:24 +0100

    SCSS und Bootstrap Ausarbeitung, Reorganisierung und paar minor fixes

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <!DOCTYPE html>
 <html lang="de">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Audiowide" type='text/css'>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="indexcss.css">
+<link rel="stylesheet" href="./css/main.css">
 
 
-<head style="font-family: 'Audiowide', sans-serif !important;">
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fakezon - Startseite</title>
@@ -27,9 +24,8 @@
             }
         };
     </script>
-</head>-
-<body style="background-color: lightgrey">
-
+</head>
+<body class="body">
     <!-- Header-Bereich mit Titel der Webseite und Suchleiste -->
     <header>
         <div class="container">
@@ -37,8 +33,8 @@
             <p>Ihr Online-Marktplatz für alles Mögliche!</p>
         </div>
         <!-- Kategorien-Buttons -->
-        <nav class="navbar navbar-expand-md bg-warning p-0 mx-auto" style="border-radius: 7px">
-            <div class="container-fluid">
+        <nav class="navbar navbar-expand-md bg-warning p-0 mx-auto">
+            <div class="container-fluid px-5 py-2">
                 <a class="navbar-brand color-black" href="index.php">Alle Produkte</a>
                 <button
                     class="navbar-toggler"
@@ -52,7 +48,7 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarID">
                     <div class="navbar-nav mr-auto">
-                        <a style="border-style: solid; border-width: 2px; border-radius: 7px" class="nav-link" href="index.php?category=Oberteile">Oberteile</a>
+                        <a class="nav-link" href="index.php?category=Oberteile">Oberteile</a>
                         <a class="nav-link" href="index.php?category=Accessoires">Accessoires</a>
                         <!-- Weitere Kategorien können hier hinzugefügt werden -->
                     </div>
@@ -112,12 +108,12 @@
 
                     // Schleife durch die Produkte
                     while ($row = $result->fetch_assoc()) {
-                        echo "<div style='background-color: #ebe6e6; width:18rem; display:inline-block;' class='card col mb-5' >";
+                        echo "<div class='card col mb-4' style='width:18rem;'>";
                         if (!empty($row['image_url'])) {
                             echo '<img class="card-img-top rounded mt-1" alt="Bild konnte nicht geladen werden" width="300" height="300" src="data:image/jpeg;base64,'.base64_encode($row['image_url']).'"/>';
                         }
                         echo "<div class='card-body'>";
-                        echo "<h2 class='card-title' style='text-align: center;'><a style='font-size: 75%; text-decoration: none; text-color: black; !important' href='product.php?id=" . $row['id'] . "' >" . htmlspecialchars($row['name']) . "</a></h2>";
+                        echo "<h2 class='card-title'><a href='product.php?id=" . $row['id'] . "' >" . htmlspecialchars($row['name']) . "</a></h2>";
                         echo "<p class='card-footer text-center'>Preis: €" . number_format($row['price'], 2) . "</p>
                             </div>";
                         echo "</div>";
@@ -136,13 +132,12 @@
 
 
     <!-- Scroll-to-Top Button -->
-    <button class="btn btn-danger btn-floating btn-lg" onclick="scrollToTop()" id="scrollToTopBtn" title="Nach oben scrollen"
-            style="display: none; position: fixed; bottom: 20px; right: 20px; z-index: 1000; padding: 10px 15px;">
+    <button class="btn btn-danger btn-floating btn-lg" onclick="scrollToTop()" id="scrollToTopBtn" title="Nach oben scrollen">
         ↑ Nach oben
     </button>
 
     <!-- Footer-Bereich -->
-    <footer class="container">
+    <footer class="container border-top border-dark py-2">
         <p>Alle Rechte vorbehalten &copy; <?php echo date("Y"); ?> Fakezon</p>
     </footer>
 
